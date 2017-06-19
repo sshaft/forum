2d40ef248deb8e577f0bcc1bf7e915a41e264d38
giacomo ceruti <ceruti.giacomo@gmail.com>
2017-06-19 18:25:59 +0200

    add Schema directory

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\facades\Schema;
+use Illuminate\Support\facades\schema;
 
 class AppServiceProvider extends ServiceProvider
 {
