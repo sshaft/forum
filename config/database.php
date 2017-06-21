0e5fde156922b7e58f718e60692cb6e01428f41d
giacomo ceruti <ceruti.giacomo@gmail.com>
2017-06-21 21:26:06 +0200

    added database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql11.freemysqlhosting.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sql11181438'),
+            'username' => env('DB_USERNAME', 'sql11181438'),
+            'password' => env('DB_PASSWORD', '9IhbhV6UKU'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
